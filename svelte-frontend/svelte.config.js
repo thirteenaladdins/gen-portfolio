89206950f847bbc6e6719e8aff825f182f0b4258
thirteenaladdins <moamrr@gmail.com>
2023-01-04 21:36:05 +0000

    adapter

--- svelte-frontend/svelte.config.js
+++ svelte-frontend/svelte.config.js
@@ -1,31 +1,31 @@
-// import adapter from '@sveltejs/adapter-auto';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+// import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		adapter: adapter()
-// 	}
-// };
-
-// export default config;
-
-export default {
+const config = {
 	kit: {
-	  // default options are shown
-	  adapter: vercel({
-		// if true, will deploy the app using edge functions
-		// (https://vercel.com/docs/concepts/functions/edge-functions)
-		// rather than serverless functions
-		edge: false,
-  
-		// an array of dependencies that esbuild should treat
-		// as external when bundling functions
-		external: [],
-  
-		// if true, will split your app into multiple functions
-		// instead of creating a single one for the entire app
-		split: false
-	  })
+		adapter: adapter()
 	}
-  };
+};
+
+export default config;
+
+// export default {
+// 	kit: {
+// 	  // default options are shown
+// 	  adapter: vercel({
+// 		// if true, will deploy the app using edge functions
+// 		// (https://vercel.com/docs/concepts/functions/edge-functions)
+// 		// rather than serverless functions
+// 		edge: false,
+  
+// 		// an array of dependencies that esbuild should treat
+// 		// as external when bundling functions
+// 		external: [],
+  
+// 		// if true, will split your app into multiple functions
+// 		// instead of creating a single one for the entire app
+// 		split: false
+// 	  })
+// 	}
+//   };
